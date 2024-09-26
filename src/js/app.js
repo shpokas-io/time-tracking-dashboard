a792d513c3348237fd578497b096a36a022f1713
shpokas <s.shpokas@gmail.com>
2024-09-26 14:56:08 +0300

    Added logic for menu initialization and finished with the function of it

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// GLOBAL VARIABLES
+let timeFrame = "weekly"; //This is default value
+const container = document.querySelector(".container");
+let regularCard; // Placeholder for all the cards
+
+//1. Make menu
+const menu = document.querySelectorAll(".menu-link");
+
+menu.forEach((element) => {
+  element.addEventListener("click", menuOnClick);
+});
+
+//2. GET JSON DATA & CREATE CARDS
+
+//--------Functions
+
+function menuOnClick(event) {
+  // console.log("click", event.target.innerText.toLowerCase());
+  menu.forEach((element) => {
+    element.classList.remove("menu-active");
+  });
+  event.target.classList.add("menu-active");
+  timeFrame = event.target.innerText.toLowerCase();
+
+  updateCards(timeFrame);
+}
+
+function updateCards(timeFrame) {}
